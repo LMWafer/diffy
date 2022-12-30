23bc9c2d6a724233d39c2dbd8f95aa32e3b74d2e
LMWafer <louismarie.restout@gmail.com>
2022-12-30 17:54:44 +0000

    Modified envloader for new ws location !

--- envloader.sh
+++ envloader.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 source /opt/ros/noetic/setup.bash
-source ~/diffy_ws/devel/setup.bash
+source ../devel/setup.bash
 
 export ROS_MASTER_URI=http://<HOST_IP>:11311
 export ROS_HOSTNAME=<MY_IP>
